dff17ef9970addfee7a50ed4c3f8b7128503c78f
zazzix <81529822+Zazzix@users.noreply.github.com>
2025-11-30 19:35:47 +0000

    feat: add hover effect

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const getSize = document.querySelector(".size")
 const canvas = document.querySelector(".canvas")
 
 
+
 getSize.addEventListener('click', () => {
 
 
-    for (i = 1; i <= 32; i++) {
+    for (let i = 1; i <= 256; i++) {
         const div = document.createElement('div')
         div.classList.add("square")
+        div.addEventListener('mouseover', () => {
+            div.classList.add("black-square")
+        })
         canvas.appendChild(div)
     }
 })
