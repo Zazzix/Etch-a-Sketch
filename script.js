873bea96f05c81ebad18666d10135e74413aedae
zazzix <81529822+Zazzix@users.noreply.github.com>
2025-12-01 18:27:52 +0000

    feat: finish project

--- script.js
+++ script.js
@@ -3,10 +3,31 @@ const canvas = document.querySelector(".canvas")
 
 
 
+
 getSize.addEventListener('click', () => {
 
+    let size = parseInt(prompt("Enter the Grid size from 1 to 100"));
 
-    for (let i = 1; i <= 256; i++) {
+    if (!isNaN(size) && size > 0 && size <= 100) {
+        createGrid(size)
+    } else {
+        size = parseInt(prompt("Enter the Grid size from 1 to 100"));
+        createGrid(size)
+    }
+
+})
+
+function createGrid(size) {
+    if (canvas.children.length > 0) {
+        canvas.textContent = ""
+    }
+    canvas.style.gridTemplateColumns = `repeat(${size}, auto)`;
+    canvas.style.gridTemplateRows = `repeat(${size}, auto)`;
+    // canvas.setAttribute("gridTemplateColumns", "repeat(size, auto)");
+    // canvas.setAttribute("gridTemplateRows", "repeat(size, auto)");
+    // grid-template-columns: repeat(16, auto);
+    // grid-template-rows: repeat(16, auto);
+    for (let i = 1; i <= (size ** 2); i++) {
         const div = document.createElement('div')
         div.classList.add("square")
         div.addEventListener('mouseover', () => {
@@ -14,4 +35,4 @@ getSize.addEventListener('click', () => {
         })
         canvas.appendChild(div)
     }
-})
+}
